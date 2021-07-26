e4ba97e3df52e40e6a83e9b3d0ad6712acd1e925
Joshua Jameson-Wallis <jjw@Joshuas-MacBook-Air.local>
2021-07-26 14:27:49 +0100

    CSS up to Testimonials

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const header = document.querySelector('#header')
 const nav = document.querySelector('#header-nav')
 
 header.addEventListener('click', e => {
-    e.preventDefault()
-    if (e.target.matches('#hamburger-icon')) nav.classList.toggle('hidden')
+    if (e.target.matches('#hamburger-icon')) { 
+        e.preventDefault()
+        nav.classList.toggle('hidden')
+    }
 })
