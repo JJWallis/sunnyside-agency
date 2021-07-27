1fd52f2f7ec1004bc80ae01713f28c6cdbbfe68a
Joshua Jameson-Wallis <jjw@Joshuas-MacBook-Air.local>
2021-07-27 11:41:58 +0100

    Updated mobile nav menu behaviour

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
 const header = document.querySelector('#header')
 const nav = document.querySelector('#header-nav')
+const innerWidth = () => window.innerWidth
 
-window.addEventListener('resize', () => {
-    if (window.innerWidth < 1000) {
-        header.addEventListener('click', e => {
-            if (e.target.matches('#hamburger-icon')) { 
-                e.preventDefault()
-                if (nav.style.display === 'none') {
-                    nav.style.display = 'block'
-                } else {
-                    nav.style.display = 'none'
-                }
-            }
-        })
+header.addEventListener('click', e => {
+    if (innerWidth() < 1000) {
+        if (e.target.matches('#hamburger-icon')) { 
+            e.preventDefault()
+            nav.classList.toggle('hidden')
+        }
     }
 })
-
