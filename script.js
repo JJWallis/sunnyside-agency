03542620263d49de6a570ad570d7c0d2e64d6acd
Joshua Jameson-Wallis <jjw@Joshuas-MacBook-Air.local>
2021-07-27 08:40:49 +0100

    Script updated for mobile nav

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 const header = document.querySelector('#header')
 const nav = document.querySelector('#header-nav')
 
-header.addEventListener('click', e => {
-    if (e.target.matches('#hamburger-icon')) { 
-        e.preventDefault()
-        nav.classList.toggle('hidden')
+window.addEventListener('resize', () => {
+    if (window.innerWidth < 1000) {
+        header.addEventListener('click', e => {
+            if (e.target.matches('#hamburger-icon')) { 
+                e.preventDefault()
+                if (nav.style.display === 'none') {
+                    nav.style.display = 'block'
+                } else {
+                    nav.style.display = 'none'
+                }
+            }
+        })
     }
 })
+
