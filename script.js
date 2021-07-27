867fcdbf6f0cf6287d44f32c98c4083641dd66bf
Joshua Jameson-Wallis <jjw@Joshuas-MacBook-Air.local>
2021-07-27 17:37:42 +0100

    Fixed navigation/media query bug

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const nav = document.querySelector('#header-nav')
 const innerWidth = () => window.innerWidth
 
 header.addEventListener('click', e => {
-    if (innerWidth() < 1000) {
+    if (innerWidth() <= 1000) {
         if (e.target.matches('#hamburger-icon')) { 
             e.preventDefault()
             nav.classList.toggle('hidden')
